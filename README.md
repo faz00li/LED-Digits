5c2473e1507864f8adfe3445375d77c014d6aa6e
fazOOli <raskine@uw.edu>
2021-05-11 01:38:03 -0400

    Add finished project

---
+++ README.md
@@ -0,0 +1,1 @@
+Program that takes in a number written as a string and prints each digit as an LED digit made of hash marks (#).
--- led.py
+++ led.py
@@ -1,25 +1,7 @@
 import sys
-################################################################################
-#   LED representations of binary digits.
-#   Dictionary of LED digits
-################################################################################
-zero = [
-    ["#", "#", "#"],
-    ["#", " ", "#"],
-    ["#", " ", "#"],
-    ["#", " ", "#"],
-    ["#", "#", "#"]
-]
+from ledigits import zero, one, two, three, four, five, six, seven, eight, nine
 
-one = [
-    [" ", " ", "#"],
-    [" ", " ", "#"],
-    [" ", " ", "#"],
-    [" ", " ", "#"],
-    [" ", " ", "#"]
-]
-
-dig_dic = { 0: zero, 1: one }
+ledig_dic = { 0: zero, 1: one, 2: two, 3: three, 4: four, 5: five, 6: six, 7: seven, 8: eight, 9: nine }
 
 ################################################################################
 #  LED related functions
@@ -30,62 +12,41 @@ def printDigit(digit: list):
     print()
 
 ################################################################################
-#   Obtain binary number in the form of a string from the CLI.
+#   Obtain number in the form of a string from the CLI.
 #   Validate input.
 ################################################################################
 args = sys.argv[1]
 if args.isdigit() == False:
-    print("Binary number must consist of 0 and 1s.")
+    print("Number must consist of digits 0 to 9.")
     exit(0)
 
-print("The binary number is: ", args)
+print("The number is: ", args)
 digits = []
-for c in args:
+# Turn string digits to integers in list in order to use led dictionary
+for c in args: 
     digits.append(int(c))
 
-print("The parsed binary number as a list is: ", digits)
+print("The parsed number as a list is: ", digits)
 
+################################################################################
+#   Program Execution
+#   Create list of digits as tuples
+################################################################################
 led_digits = []
 for i in range(len(digits)):
-    x = tuple(dig_dic[digits[i]])
+    x = tuple(ledig_dic[digits[i]])
     led_digits.append(x)
-
-print(led_digits)
 
 segment = ""
 line = ""
 
-rank = 0
+print("Number of digits: ", len(digits))
 for rank in range(5):
-    print("Number of digits: ", len(digits))
     for i in range(len(digits)):
         x = led_digits[i][rank]
-        print(x)
-        segment = segment + line.join(x)
+        segment = segment + " " + str(x).strip("[']")
     print(segment)
-    print()
     segment = ""
     line = ""
 
-# rank = 1
-# for i in range(len(digits)):
-#     print(led_digits[i][rank], end=".")
-# print()
-
-# rank = 2
-# for i in range(len(digits)):
-#     print(led_digits[i][rank], end=".")
-# print()
-
-
-
-
-
-
-################################################################################
-#   Program Execution
-################################################################################
-
-
-# Parse args - seperate each character
 
